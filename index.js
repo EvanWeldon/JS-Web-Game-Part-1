9844549c2697787b1dc2da024c52d54eeb0ab42a
EvanWeldon <WeldonEvanT@gmail.com>
2022-11-18 21:54:13 -0800

    created the newItem function with dbl click event listener and invoked sword, shield and staff in the newItem function

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ function newImage(url, left, bottom){
     return item
 }
 
+//creating a function for newItem to create a new unit and add an event listener for each unit
+
+function newItem(url, left, bottom){
+    let unit = newImage(url, left, bottom)
+
+    unit.addEventListener('dblclick', () => {
+        unit.remove()
+    })
+}
+
 //invoke the function for each of the 4) new images
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
@@ -17,3 +27,8 @@ newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+//invoke the function for each of the 3 items to be added
+newItem('assets/sword.png', 500, 405)
+newItem('assets/staff.png', 600, 100)
+newItem('assets/sheild.png',165, 185)
