e055078bd6024cb701ffaaa3d3da4487bb42e21d
EvanWeldon <WeldonEvanT@gmail.com>
2022-11-18 21:28:49 -0800

    created the newImage function and added all of the invokes for new images

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//function to create new items with specific positions
+
+function newImage(url, left, bottom){
+    let item = document.createElement('img')
+    item.src = url
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
+    return item
+}
+
+//invoke the function for each of the 4) new images
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
